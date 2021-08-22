070178ea02f850bade1eb1e915860a91324ddfaa
Miq313 <miq313@hotmail.com>
2021-08-22 14:59:33 -0400

    bug fixes

--- Main.py
+++ Main.py
@@ -1,10 +1,5 @@
 import SerWrite
 
 #Run SerRead.py in a seperate thread
-x = 0
-while x!=10000:
-    SerWrite.serWrite("F:200")
-    SerWrite.serWrite("B:200")
-    SerWrite.serWrite("R:200")
-    SerWrite.serWrite("L:200")
-    x+=1
+SerWrite.serWrite("F:200")
+SerWrite.serWrite("B:200")
