24d84e8bd1624e0b3abd66fa2b72ab0918babdfa
Miqdaad Khimji <miq313@hotmail.com>
2022-08-27 01:49:18 -0400

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo apt-get update
+sudo apt install build-essential software-properties-common -y
 
-# Install python3.9
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get install python3.9
+sudo apt-add-repository ppa:deadsnakes/ppa -y
+sudo apt update -y && sudo apt full-upgrade -y
+sudo apt install python3.9 python3.9-dev python3.9-venv -y
+python3.9 -m pip install --upgrade pip
 
 # Create python venv and install dependencies
 python3.9 -m venv venv
