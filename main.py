9858dbe8853af3a254af206c196487ca85f8c2b1
Miqdaad Khimji <miq313@hotmail.com>
2022-08-27 19:40:41 -0400

    updated main.py

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from scripts.cartography import create_map
 from scripts.localization import get_cur_pos, update_cur_pos, reset_cur_pos
 from scripts.navigation import navigate
 
+# Reading config file
 config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
 with open(config_file_path, "r") as config_file:
     config = json.load(config_file)
     serial_port = config["communications"]["serialPort1"]
+    room = config["current_room"]
 
+# Initialize serial connection to PicoControl
 ser = serial.Serial(serial_port, 9600, timeout=1)
 ser.flush()
 
@@ -19,6 +22,9 @@ def serial_write(command):
     command_bytes = bytes(command + ";", 'utf-8')
     ser.write(command_bytes)
     print("Sent " + command + " to  PicoControl")
+
+# User input to determine mode
+mode = input("Enter mode ('learning' or 'running'): ")
 
 # Subprocess for reading serial output & updating MPS
 def process_serial_monitor():
@@ -34,9 +40,20 @@ def process_serial_monitor():
 
 # Subprocess for controlling the robot
 def process_main():
-    # choose destination, find path, send commands to PicoControl
-    serial_write("F:200")
-    serial_write("B:200")
+    if mode == "learning":
+        # run random navigation script
+        # Create map
+        print("Creating map of " + room)
+        create_map(room)
+    elif mode == "running":
+        while True:
+            # find a place to go
+            path = navigate()
+            # convert path to instructions
+            instructions = ""
+            serial_write(instructions)
+            # check if robot is homed
+            # reset_cur_pos() if homed
 
 process1 = multiprocessing.Process(target=process_serial_monitor)
 process2 = multiprocessing.Process(target=process_main)
